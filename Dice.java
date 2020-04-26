ed135c353109007b4887cae63cc1d41dfb1defd0
KuroYasha00 <64342529+KuroYasha00@users.noreply.github.com>
2020-04-26 16:14:48 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,1 +1,15 @@
-class Dice
+import static java.lang.Math.random();
+public class Dice{
+  private int topFace;//declaring one int var called topFace
+  
+  public Dice(){//constructor will assign a value to topFace and then return it
+    RollDice();
+    GetDice();
+  }
+  public void RollDice(){//assigns a random number to topFace of dice of numbers 1 to 6
+    topFace = (int)(Math.random()*6)+1;
+  }
+  public int GetDice(){//will retrun the integer of the top face of the dice
+    return topFace;
+  }
+}// end of class Dice
