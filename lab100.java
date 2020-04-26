19265857a126d347f64059c3a411daaefaa8e196
KuroYasha00 <64342529+KuroYasha00@users.noreply.github.com>
2020-04-26 16:59:23 -0700

    Update lab100.java

--- lab100.java
+++ lab100.java
@@ -1,1 +1,11 @@
-
+package https://github.com/KuroYasha00/lab10cecs174;
+import java.util.Scanner; need this to get input
+public class lab100{
+  static Scanner S= new Scanner(System.in)
+  
+  public static void main(String[] args) {
+  Player p1 = new Player("mark", 200)
+  Player p2 = new Player("polo", 200)
+  
+  }
+}
