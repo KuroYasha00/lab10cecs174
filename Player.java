2013d09d5fdb28ead0976f0752bdcabe47d13084
KuroYasha00 <64342529+KuroYasha00@users.noreply.github.com>
2020-04-26 16:41:42 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,33 @@
 
+public class Player{
+  public String name;
+  public int money;
+  public int Dice;
+  
+  public Player(String name, int money){
+    this.name = name;
+    this.money = money;
+    Roll();
+    
+  }
+  public void Roll(){
+    Dice.RollDice();
+    Dice = Dice.GetDice();
+  }
+  public int Lose(){
+    int minus = 50;
+    money = money - minus;
+    return money;
+  }
+  public int GetMoney(){
+    return money;
+  }
+  public String GetName(){
+    return name;
+  }
+  public int Win(){
+    int plus = 50;
+    money = money + plus;
+    return money
+  }
+}
