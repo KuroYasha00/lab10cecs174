3d8b831b61336b06e47b7c467555d1ec42b28564
KuroYasha00 <64342529+KuroYasha00@users.noreply.github.com>
2020-04-26 17:51:52 -0700

    Update lab100.java

--- lab100.java
+++ lab100.java
@@ -1,11 +1,26 @@
-package https://github.com/KuroYasha00/lab10cecs174;
-import java.util.Scanner; need this to get input
 public class lab100{
-  static Scanner S= new Scanner(System.in)
-  
-  public static void main(String[] args) {
-  Player p1 = new Player("mark", 200)
-  Player p2 = new Player("polo", 200)
-  
-  }
+    static Scanner S= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Player p1 = new Player("mark", 200);
+        Player p2 = new Player("com", 200);
+        int p1die, p2die, bet;
+        p1die = p1.Roll();
+        p2die = p2.Roll();
+        System.out.println("Hello player "+p1.GetName()+" you have a balance of "+p1.GetMoney());
+        System.out.println("How much do you want to bet?");
+        bet = S.nextInt();
+        System.out.println(p1.GetName()+" rolled "+p1die+"\n"+p2.GetName()+" rolled "+p2die);
+        if (p1die > p2die){
+            p1.Win(bet);
+            System.out.println(p1.GetName()+" won.\nYour new balance is "+p1.GetMoney());
+        }
+        if (p1die < p2die){
+            p2.Win(bet);
+            System.out.println(p1.GetName()+" lost.\nYour new balance is "+p1.GetMoney());
+        }
+        if (p1die == p2die){
+            System.out.println("it is a tie");
+        }
+    }
 }
